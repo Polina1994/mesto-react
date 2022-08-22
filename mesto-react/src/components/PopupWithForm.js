0f3f06565b37f60810ba30e719d8bb6b6fd1d4c0
Polina Ka <polinako1994@gmail.com>
2022-08-22 10:25:20 +0900

    Create PopupWithForm.js

---
+++ mesto-react/src/components/PopupWithForm.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function PopupWithForm({name, title, children, isOpen, btnName, onClose = false}) {
+
+  return (
+    <div className={`popup popup_type_${name}
+    ${isOpen ? 'popup_is-opened' : false}`}> 
+        <div className="popup__container">
+            <button type="button" className={`popup__close-button`} onClick={onClose}></button>
+            <h2 className="popup__title">{title}</h2>
+
+            <form className="popup__form" name={`${name}`} id={`${name}`} noValidate>
+                <>{children}</>
+                <button type="submit" className="popup__button">{btnName}</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default PopupWithForm;
