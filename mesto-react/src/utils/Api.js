ba8209991489ef07865a61cb1fef7d4fb15920b0
Polina Ka <polinako1994@gmail.com>
2022-08-22 10:26:28 +0900

    Create Api.js

---
+++ mesto-react/src/utils/Api.js
@@ -0,0 +1,98 @@
+class Api {
+  constructor({ headers, baseUrl }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+
+  getUserData() {
+    const requestUrl = this._baseUrl + "/users/me";
+    return fetch(requestUrl, {
+      headers: this._headers,
+    }).then(this._getResponseData);
+  }
+
+
+  getCards() {
+    const requestUrl = this._baseUrl + "/cards";
+    return fetch(requestUrl, {
+      headers: this._headers,
+    }).then(this._getResponseData);
+  }
+
+  setUserData(data) {
+    const requestUrl = this._baseUrl + "/users/me";
+    return fetch(requestUrl, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: data.profile__name,
+        about: data.profile__job,
+      }),
+    }).then(this._getResponseData);
+  }
+
+  addNewCard(data) {
+    const requestUrl = this._baseUrl + "/cards";
+    return fetch(requestUrl, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: data.name,
+        link: data.link,
+      }),
+    }).then(this._getResponseData);
+  }
+
+  deleteCard(data) {
+    const requestUrl = this._baseUrl + `/cards/${data._id}`;
+    return fetch(requestUrl, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._getResponseData);
+  }
+
+  likeCard(cardId) {
+    const requestUrl = this._baseUrl + `/cards/likes/${cardId}`;
+    return fetch(requestUrl, {
+      method: "PUT",
+      headers: this._headers,
+    }).then(this._getResponseData);
+  }
+
+  dislikeCard(cardId) {
+    const requestUrl = this._baseUrl + `/cards/likes/${cardId}`;
+    return fetch(requestUrl, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._getResponseData);
+  }
+
+  setNewAvatar(data) {
+    const requestUrl = this._baseUrl + `/users/me/avatar`;
+    return fetch(requestUrl, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: data.avatar_link,
+      }),
+    }).then(this._getResponseData);
+  }
+
+  _getResponseData(res) {
+    if (res.ok) {
+      return res.json();
+    } else {
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }
+  }
+}
+
+const api = new Api({
+  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-46",
+  headers: {
+    authorization: "b728ad0c-8a51-4521-be05-924ef3deb392",
+    "Content-Type": "application/json",
+  },
+});
+export default api;
