6ade2a265f938f0d8de910b0212dbeaa7d2c3806
Polina Ka <polinako1994@gmail.com>
2022-08-29 16:24:35 +0900

    Update PopupWithForm.js

--- mesto-react/src/components/PopupWithForm.js
+++ mesto-react/src/components/PopupWithForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function PopupWithForm({name, title, children, isOpen, btnName, onClose = false}) {
+function PopupWithForm({name, title, children, isOpen, btnName, onClose, onSubmit}) {
 
   return (
     <div className={`popup popup_type_${name}
@@ -9,7 +9,7 @@ function PopupWithForm({name, title, children, isOpen, btnName, onClose = false}
             <button type="button" className={`popup__close-button`} onClick={onClose}></button>
             <h2 className="popup__title">{title}</h2>
 
-            <form className="popup__form" name={`${name}`} id={`${name}`} noValidate>
+            <form className="popup__form" name={`${name}`} id={`${name}`} onSubmit={onSubmit} noValidate>
                 <>{children}</>
                 <button type="submit" className="popup__button">{btnName}</button>
             </form>
