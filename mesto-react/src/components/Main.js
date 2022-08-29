5e3a3874ae2ef0cd2327ccaef7f007e10969a0a7
Polina Ka <polinako1994@gmail.com>
2022-08-29 16:24:38 +0900

    Update Main.js

--- mesto-react/src/components/Main.js
+++ mesto-react/src/components/Main.js
@@ -1,21 +1,26 @@
 import React from "react";
 import Card from "./Card";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
-function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick, cards, userDescription, userName, userAvatar}) {
+function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick, onCardLike, onCardDelete, cards}) {
 
+    const currentUser = React.useContext(CurrentUserContext)
+
+   
     return (
+        <CurrentUserContext.Provider value={currentUser}>
         <main className="content">
 
             <section className="profile">
                 <div className="profile__gak">
                     <div className="profile__avatar-wrapper">
-                        <img className="profile__avatar" alt="аватар профиля" src={userAvatar.avatar}/>
+                        <img className="profile__avatar" alt="аватар профиля" src={currentUser.avatar}/>
                         <button className="profile__avatar-overlay" onClick={onEditAvatar}></button>
                     </div> 
                     <div className="profile__info">
-                        <h1 className="profile__name">{userName.name}</h1>
+                        <h1 className="profile__name">{currentUser.name}</h1>
                         <button className="open-popup profile__edit-button" type="button" onClick={onEditProfile}></button>
-                        <p className="profile__job">{userDescription.about}</p>
+                        <p className="profile__job">{currentUser.about}</p>
                     </div>
                 </div>
                 <button className="open-popup profile__add-button" type="button" onClick={onAddPlace}></button>
@@ -26,10 +31,13 @@ function Main({onEditProfile, onAddPlace, onEditAvatar, onCardClick, cards, user
                 <Card key={card._id}
                         card={card} 
                         onCardClick={onCardClick}
+                        onCardLike={onCardLike}
+                        onCardDelete={onCardDelete}
                         />)
             )}
             </section>
          </main>
+         </CurrentUserContext.Provider>
     )
 }
 
