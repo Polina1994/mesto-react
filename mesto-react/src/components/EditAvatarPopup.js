0d0730142beb3ade17f22a10d7a61f61e5aca62f
Polina Ka <polinako1994@gmail.com>
2022-08-29 16:24:49 +0900

    Create EditAvatarPopup.js

---
+++ mesto-react/src/components/EditAvatarPopup.js
@@ -0,0 +1,50 @@
+import PopupWithForm from "./PopupWithForm";
+import {useEffect, useState} from 'react'
+
+function EditAvatarPopup({isOpen, onClose, onUpdateAvatar}) {
+
+    const [avatar, setAvatar] = useState('')
+
+    function handleSubmit(e) {
+        e.preventDefault();
+      
+        onUpdateAvatar({avatar})
+      }
+
+    function handleChange(e) {
+        const input = e.target
+        setAvatar(input.value)
+    }
+
+    useEffect(() => {
+        if(isOpen) {
+        setAvatar('')
+        }
+    }, [isOpen])
+
+
+    return (
+        <PopupWithForm
+        title='Обновить аватар'
+        name='formAvatar'
+        btnName='Сохранить'
+        isOpen={isOpen}
+        onClose={onClose}
+        onSubmit={handleSubmit}
+        >
+        <input 
+        type="url" 
+        name="avatar" 
+        id="form__input-avatar" 
+        form="formAvatar" 
+        className="popup__input popup__input_value_avatar form__input" 
+        placeholder="Ссылка"
+        value={avatar}
+        onChange={handleChange}
+        required/>
+        <span className="form__input-avatar-error"></span>
+        </PopupWithForm>
+    )
+}
+
+export default EditAvatarPopup;
