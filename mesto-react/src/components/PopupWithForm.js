8eb52dacacb93f8b9bfdf3997b8c988d20f31113
Polina Ka <polinako1994@gmail.com>
2022-09-02 14:30:53 +0900

    Update PopupWithForm.js

--- mesto-react/src/components/PopupWithForm.js
+++ mesto-react/src/components/PopupWithForm.js
@@ -1,21 +1,41 @@
 import React from "react";
 
-function PopupWithForm({name, title, children, isOpen, btnName, onClose, onSubmit}) {
-
+function PopupWithForm({
+  name,
+  title,
+  children,
+  isOpen,
+  btnName,
+  onClose,
+  onSubmit,
+}) {
   return (
-    <div className={`popup popup_type_${name}
-    ${isOpen ? 'popup_is-opened' : false}`}> 
-        <div className="popup__container">
-            <button type="button" className={`popup__close-button`} onClick={onClose}></button>
-            <h2 className="popup__title">{title}</h2>
+    <div
+      className={`popup popup_type_${name}
+    ${isOpen ? "popup_is-opened" : false}`}
+    >
+      <div className="popup__container">
+        <button
+          type="button"
+          className={`popup__close-button`}
+          onClick={onClose}
+        ></button>
+        <h2 className="popup__title">{title}</h2>
 
-            <form className="popup__form" name={`${name}`} id={`${name}`} onSubmit={onSubmit} noValidate>
-                <>{children}</>
-                <button type="submit" className="popup__button">{btnName}</button>
-            </form>
-        </div>
+        <form
+          className="popup__form"
+          name={`${name}`}
+          id={`${name}`}
+          onSubmit={onSubmit}
+        >
+          <>{children}</>
+          <button type="submit" className="popup__button">
+            {btnName}
+          </button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
 export default PopupWithForm;
