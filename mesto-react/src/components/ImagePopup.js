a35dc25ce8c98b5bd1d5a84646f74f583bd1b1f6
Polina Ka <polinako1994@gmail.com>
2022-09-02 14:30:59 +0900

    Update ImagePopup.js

--- mesto-react/src/components/ImagePopup.js
+++ mesto-react/src/components/ImagePopup.js
@@ -1,13 +1,25 @@
 function ImagePopup(props) {
-    return(
-        <div className={`popup popup-image ${props.card.name ? 'popup_is-opened' : ''}`}>
-        <article className="popup-image__element">
-            <button type="button" className="popup__close-button popup-image__close-button" onClick={props.onClose}></button>
-            <img src={props.card.link} className="popup-image__image" alt={props.card.name}/>
-            <h2 className="popup-image__title">{props.card.name}</h2>
-        </article>
-    </div> 
-    )
+  return (
+    <div
+      className={`popup popup-image ${
+        props.card.name ? "popup_is-opened" : ""
+      }`}
+    >
+      <article className="popup-image__element">
+        <button
+          type="button"
+          className="popup__close-button popup-image__close-button"
+          onClick={props.onClose}
+        ></button>
+        <img
+          src={props.card.link}
+          className="popup-image__image"
+          alt={props.card.name}
+        />
+        <h2 className="popup-image__title">{props.card.name}</h2>
+      </article>
+    </div>
+  );
 }
 
 export default ImagePopup;
