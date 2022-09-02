324599548a571a2b3c415680f34f2a0f08e3fbb2
Polina Ka <polinako1994@gmail.com>
2022-09-02 14:31:01 +0900

    Update Header.js

--- mesto-react/src/components/Header.js
+++ mesto-react/src/components/Header.js
@@ -1,13 +1,12 @@
-import headerLogo from '../../src/images/logo.svg'
-import React from 'react'
+import headerLogo from "../../src/images/logo.svg";
+import React from "react";
 
 function Header() {
-    return (
-
+  return (
     <header className="header">
-        <img className="header__image" src={headerLogo} alt="лого"/>
+      <img className="header__image" src={headerLogo} alt="лого" />
     </header>
-    )
+  );
 }
 
 export default Header;
